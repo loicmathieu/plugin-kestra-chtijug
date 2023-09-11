689ff71cc96a8d8b6896ba46b402e81d87038b94
Loïc Mathieu <loikeseke@gmail.com>
2023-09-11 15:42:24 +0200

    Upgrade to Kestra 0.15 and Micronaut 4.3

---
+++ src/main/java/io/kestra/plugin/chtijug/Trigger.java
@@ -0,0 +1,48 @@
+package io.kestra.plugin.chtijug;
+
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.conditions.ConditionContext;
+import io.kestra.core.models.executions.Execution;
+import io.kestra.core.models.triggers.AbstractTrigger;
+import io.kestra.core.models.triggers.PollingTriggerInterface;
+import io.kestra.core.models.triggers.TriggerContext;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.time.Duration;
+import java.util.Optional;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "Plugin example"
+)
+@Plugin(
+    examples = {
+        @Example(
+            title = "Plugin example",
+            code = {
+                """
+                    TODO"""
+            }
+        )
+    }
+)
+public class Trigger extends AbstractTrigger implements PollingTriggerInterface {
+    @Builder.Default
+    private final Duration interval = Duration.ofSeconds(60);
+
+    @Override
+    public Optional<Execution> evaluate(ConditionContext conditionContext, TriggerContext context) throws Exception {
+        return Optional.empty();
+    }
+}
